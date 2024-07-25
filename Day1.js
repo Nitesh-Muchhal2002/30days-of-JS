544ba520a6f43bc9445f9fdbec15c2310c521e41
Nitesh <nitsmuchhal@gmail.com>
2024-07-25 17:57:36 +0530

    this is a day1 challenge

---
+++ Day1.js
@@ -0,0 +1,33 @@
+//variable and data type
+//variable let var const
+//type number string boolean object
+
+
+var a=56;
+console.log(a);
+let b="nitehs";
+console.log(b);
+const c=true;
+console.log(c);
+
+let num=56;
+console.log(num);
+
+  num=90;
+  console.log(num)
+
+  //data type
+let s="nitesh";
+console.log(typeof s);
+
+let arr=[9,0,5,6];
+console.log(typeof arr);
+
+let obj={
+    name:"nites"
+}
+console.log(typeof obj);
+
+const d=89;
+d=0;
+console.log(d)
