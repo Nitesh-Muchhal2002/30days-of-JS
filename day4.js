cf3bcfdd74977f7b8ccee8bd6114789166d79d9f
Nitesh Muchhal <130092906+Nitesh-Muchhal2002@users.noreply.github.com>
2024-07-31 19:11:13 +0530

    changes day4.js

--- day4.js
+++ day4.js
@@ -1,65 +1,65 @@
 // functions
 // odd and even no
 
-//  function oddEven(num)
-//  {
-//     if(num%2==0)
-//     {
-//         console.log("num is even");
+function oddEven(num)
+ {
+    if(num%2==0)
+    {
+        console.log("num is even");
     
-//     }
-//     else{
-//         console.log("num is odd")
-//     } 
-//  }
-// oddEven(48);
+    }
+    else{
+        console.log("num is odd")
+    } 
+ }
+oddEven(48);
 
 //  square
-// function square(num)
-// {
-//     let result=num*num;
-//     console.log(result)
-// }
-// square(9)
+function square(num)
+{
+    let result=num*num;
+    console.log(result)
+}
+square(9)
 
 // ///max of two number
-// function maximum(num1,num2)
-// {
-//     if(num1>num2)
-//     {
-//         return num1;
-//     }
-//     else{
-//         return num2;
-//     }
-// }
-// console.log(maximum(4,6));
+function maximum(num1,num2)
+{
+    if(num1>num2)
+    {
+        return num1;
+    }
+    else{
+        return num2;
+    }
+}
+console.log(maximum(4,6));
 
 
-// function concanate(str1,str2)
-// {
-//      let result =str1+str2;
-//     let result=str1.concat(str2)
-//      return result;
-// }
-// console.log(concanate("nitesh","kabir"));
+function concanate(str1,str2)
+{
+     let result =str1+str2;
+    let result=str1.concat(str2)
+     return result;
+}
+console.log(concanate("nitesh","kabir"));
 
   
 // //arrow Function
-//  const sum=(a,b)=>a+b;
-//  console.log(sum(7,4))
+ const sum=(a,b)=>a+b;
+ console.log(sum(7,4))
 
-// const contains=(str,char)=>str.includes(char);
-// console.log(contains('Hello','u'));
+const contains=(str,char)=>str.includes(char);
+console.log(contains('Hello','u'));
 
-// const fun=(a,b=5)=>a*b;
-// console.log(fun(4));
+const fun=(a,b=5)=>a*b;
+console.log(fun(4));
 
-// function message(name,age=22)
-// {
-//     console.log(`name is ${name} and age is ${age}`);
-// }
-// message("nitesh");
+function message(name,age=22)
+{
+    console.log(`name is ${name} and age is ${age}`);
+}
+message("nitesh");
  
 //for each
 let name=["pune","banglore","mumbai"]
