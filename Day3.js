bdb791932ae2be5af6cf80bcf9ccac535355d0a0
Nitesh Muchhal <130092906+Nitesh-Muchhal2002@users.noreply.github.com>
2024-07-31 19:09:19 +0530

    Update Day3.js

--- Day3.js
+++ Day3.js
@@ -1,55 +1,56 @@
-// //LOOP
 
-// //programe prin number using for loop 1 t0 10;
-// let num=10;
-// for (let index = 1; index <=num; index++) {
+   //while loop
+  // sum 1 to 10
+   let sum=0;
+   let index=1;// 
+//LOOP
+
+//programe prin number using for loop 1 t0 10;
+let num=10;
+for (let index = 1; index <=num; index++) {
     
-//     console.log(index);
+    console.log(index);
     
-// }
+}
 
-// //table 5
-//  let num1=5;
-//  for(let i=1;i<=10;i++)
-//  {
-//     console.log(num1*i);
-//  }
+//table 5
+ let num1=5;
+ for(let i=1;i<=10;i++)
+ {
+    console.log(num1*i);
+ }
 
-//    //while loop
-//   // sum 1 to 10
-//    let sum=0;
-//    let index=1;
-//    while(index<=10)
-//    {
-//       sum+=index;
-//       index++;
-//    }
-//    console.log(sum);
+   while(index<=10)
+   {
+      sum+=index;
+      index++;
+   }
+   console.log(sum);
    
-// // print 10 to 1
-//  let i=10;
-//  while(i>0)
-//  {
-//     console.log(i);
-//     i--;
-//  }
+// print 10 to 1
+ let i=10;
+ while(i>0)
+ {
+    console.log(i);
+    i--;
+ }
 
-// //  do while loop
-// //  print 1 to 5
-// let i=1;
-// do{
-//     console.log(i);
-//     i++;
-// }while(i<=5)
+//  do while loop
+//  print 1 to 5
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<=5)
 
-// // factorial no
+// factorial no
  
-//  let n=4,i=1,fact=1;
-//  do{
-//      fact=fact*i;
-//      i++;
-//  }while(i<=4)
-//     console.log(fact)
+ let n=4,i=1,fact=1;
+ do{
+     fact=fact*i;
+     i++;
+ }while(i<=4)
+    console.log(fact)
 
 
 
